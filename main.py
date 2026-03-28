2aff0de1b80d3717cd9564032fdd66eb08d1fcad
Yaswanth B <143199040+Yaswanthbhathrasamy@users.noreply.github.com>
2026-03-28 13:42:09 +0530

    Update banner to include version v2.0

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ _BANNER = r"""
   ██╔══██║██║   ██║██╔══╝  ██║╚██╗██║   ██║   ╚════██║
   ██║  ██║╚██████╔╝███████╗██║ ╚████║   ██║   ███████║
   ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝[/bright_cyan]
-  [dim bright_red]◤ Autonomous Offensive Reconnaissance Engine ◢[/dim bright_red]
+  [dim bright_red]◤ Autonomous Offensive Reconnaissance Engine [v2.0][/dim bright_red]
   [dim]━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━[/dim]
   [bright_magenta]                              dev by yashh[/bright_magenta]
 """
